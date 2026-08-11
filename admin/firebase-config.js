a69831104328800ef0354812eacde5afaa950590
Bodyahmed77 <sumoslmer47@gmail.com>
2026-08-12 01:38:31 +0300

    feat: add Firebase config for admin dashboard

---
+++ admin/firebase-config.js
@@ -0,0 +1,10 @@
+// Public Firebase web config. Security is enforced by Firestore Rules.
+export const firebaseConfig = {
+  apiKey: "AIzaSyANEXYUVqaGss1i9WS5gH7Ic3UrBgKG_qc",
+  authDomain: "fittrack-698fa.firebaseapp.com",
+  projectId: "fittrack-698fa",
+  storageBucket: "fittrack-698fa.firebasestorage.app",
+  messagingSenderId: "632925500741",
+  appId: "1:632925500741:web:1d42d331f0bd09f4c67a2c",
+  measurementId: "G-7S75NTCV5B",
+};
